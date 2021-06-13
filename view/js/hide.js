3ea44005840002b47612ddf04c52267864a7b52f
donataskitm <79906134+donataskitm@users.noreply.github.com>
2021-06-13 12:28:42 +0300

    Delete view/js directory

--- view/js/hide.js
+++
@@ -1,7 +0,0 @@
-
-//(document).ready(function() {
-//    ("#myform").submit(function(e) {
- //       ("#first").hide();
-  //      ("#second").show();
- //   });
-//});  //   action="/view/ticket.view.php"
